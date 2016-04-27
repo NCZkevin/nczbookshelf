ee2f1b253927c8158d425fe52ae4eb491e602e30
nczkevin <z1376871863@163.com>
2016-04-27 23:27:46 +0800

    初始化 首页

---
+++ app/controllers/home.js
@@ -0,0 +1,49 @@
+var express = require('express'),
+  router = express.Router(),
+  mongoose = require('mongoose'),
+  superagent = require('superagent'),
+  Article = mongoose.model('Article');
+
+module.exports = function (app) {
+  app.use('/', router);
+};
+
+router.get('/', function (req, res, next) {
+  var dbid = "1084336";
+  var dburl = "https://api.douban.com/v2/book/" + dbid ;
+
+  superagent.get(dburl)
+    .end(function (err,sres) {
+      var bookinfo = sres.text;
+      var book = {
+        author: sres.body.author,
+        title: sres.body.tags[0].title,
+        publisher: sres.body.publisher
+      };
+      res.render('index',{
+          title:'豆瓣book',
+          info:sres.body
+       }
+     );
+    });
+});
+
+router.get('/admin', function (req, res, next) {
+  var dbid = "1084336";
+  var dburl = "https://api.douban.com/v2/book/" + dbid ;
+
+  superagent.get(dburl)
+    .end(function (err,sres) {
+      var bookinfo = sres.text;
+      var book = {
+        author: sres.body.author,
+        title: sres.body.tags[0].title,
+        publisher: sres.body.publisher
+      };
+      res.render('admin/admin',{
+          title:'豆瓣book',
+          info:sres.body
+       }
+     );
+    });
+});
